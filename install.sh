03e22917c5518344e1dae1bf1725901dfddfeef6
trainlos <tahoedude101@gmail.com>
2023-04-19 11:49:55 -0500

    Don't auto delete setup files

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
-rm .gitattributes README.md
-echo "Switching to Debian Testing..."
-sudo mv sources.list /etc/apt/sources.list
+#!/bin/bash
+echo "Switching to Debian Bookworm..."
+sudo cp sources.list /etc/apt/sources.list
 sudo apt update
 sudo apt full-upgrade -y
 echo "Installing yt-dlp and dependencies..."
 sudo apt install ffmpeg python3 yt-dlp -y
-echo "Setting up download scripts..."
-sudo mv dl-a /usr/local/bin
-sudo mv dl-v /usr/local/bin
-sudo chmod u+x /usr/local/bin/dl-a
-sudo chmod u+x /usr/local/bin/dl-v
-rm install.sh
-echo "Setup has finished."
+echo "Setting up download script..."
+sudo cp ph-dl /usr/local/bin
+sudo chmod u+x /usr/local/bin/ph-dl
+echo "Setup has finished. These files can now be deleted."
